cc6025523c5d96bf24ee5fa736b793462363e02a
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-11-04 14:56:54 +0300

    Added Task file

--- Vault_Hacking_Race/src/MainProgram.java
+++ Vault_Hacking_Race/src/MainProgram.java
@@ -1,6 +1,21 @@
 public class MainProgram {
     public static void main(String[] args){
 
+        //Creating Vault with random pass
+        Vault vault = new Vault();
 
+        //Creating the 3 threads
+        Thread ascendingHacker = new AscendingHackerThread(vault);
+        Thread descendingHacker = new DescendingHackerThread(vault);
+        Thread police = new PoliceThread();
+
+        // setting hacker's priorities
+        ascendingHacker.setPriority(Thread.MAX_PRIORITY);
+        descendingHacker.setPriority(Thread.MAX_PRIORITY);
+
+        // launching all three threads
+        ascendingHacker.start();
+        descendingHacker.start();
+        police.start();
     }
 }
