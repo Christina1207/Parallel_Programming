4f6d1303ed099b5f590caa43064b3c639a982e06
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-11-14 19:15:15 +0300

    Added functions to count success and failure in remove thread

--- Concurrent_Sorted_Linked_List/src/test/java/RemoveThread.java
+++ Concurrent_Sorted_Linked_List/src/test/java/RemoveThread.java
@@ -3,10 +3,26 @@ public class RemoveThread extends TestThread implements Runnable{
         super(seq, seqPart, setList);
     }
 
+    private   int totalRemoveSuccessCount = 0;
+    private   int totalRemoveFailureCount = 0;
+
+
     @Override
     public void run() {
         for (int i = 0; i < nums.length; i++) {
-            list.remove(nums[i]);
+            if (list.remove(nums[i])) {
+                totalRemoveSuccessCount++;
+            } else {
+                totalRemoveFailureCount++;
+            }
         }
+    }
+
+    public  int getTotalRemoveSuccessCount() {
+        return totalRemoveSuccessCount;
+    }
+
+    public  int getTotalRemoveFailureCount() {
+        return totalRemoveFailureCount;
     }
 }
