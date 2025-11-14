90fdcfa30a6f6b1b148a2aa672c2f05965ca6b2e
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-11-14 19:12:58 +0300

    Added functions to count success and failure

--- Concurrent_Sorted_Linked_List/src/test/java/ContainThread.java
+++ Concurrent_Sorted_Linked_List/src/test/java/ContainThread.java
@@ -1,4 +1,7 @@
 public class ContainThread extends TestThread implements Runnable{
+
+    private int totalFailureCount = 0;
+    private int totalSuccessCount = 0;
     public ContainThread(RandomSeq seq, int seqPart, SortList setList) {
         super(seq, seqPart, setList);
     }
@@ -6,7 +9,22 @@ public class ContainThread extends TestThread implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i < nums.length; i++) {
-            list.contain(nums[i]);
+            // Call the list's contain method and check the result
+            if(list.contain(nums[i]))
+            {
+                totalSuccessCount++;
+            }
+            else{
+                totalFailureCount++;
+            }
         }
+    }
+
+    public int getSuccessCount() {
+        return totalSuccessCount;
+    }
+
+    public int getFailureCount() {
+        return totalFailureCount;
     }
 }
