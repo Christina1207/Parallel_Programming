ea3ed21fd6329158d66505defa7ec1854673fad2
Christina Kh <101043589+Christina1207@users.noreply.github.com>
2025-11-29 16:16:25 +0300

    Testing the threadpool execute in Main

--- Thread_Pool/src/Main.java
+++ Thread_Pool/src/Main.java
@@ -1,7 +1,20 @@
 
 public class Main {
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args) throws Exception {
+        ThreadPool threadPool = new ThreadPool(3, 5);
+        for(int i=0; i<10;i++){
+            int taskNo=i;
+            threadPool.execute(()->{
+                String message = Thread.currentThread().getName() + ": Task " + taskNo;
+                System.out.println(message);
+                try {
+                    Thread.sleep(1000);
+                }
+                catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+        }
+        System.out.println("All tasks submitted.");
     }
 }
